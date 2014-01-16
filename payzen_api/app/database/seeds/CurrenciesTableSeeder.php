05eba87510a33876a6af6d62c724a0f3a809b811
Alain Dubrulle <alain.dubrulle@lyra-network.com>
2014-01-16 18:56:22 +0100

    Euro in database

--- payzen_api/app/database/seeds/CurrenciesTableSeeder.php
+++ payzen_api/app/database/seeds/CurrenciesTableSeeder.php
@@ -2,17 +2,19 @@
 
 class CurrenciesTableSeeder extends Seeder {
 
-	public function run()
-	{
-		// Uncomment the below to wipe the table clean before populating
-		// DB::table('currencies')->truncate();
+    public function run() {
+        // Uncomment the below to wipe the table clean before populating
+        DB::table('currencies')->truncate();
 
-		$currencies = array(
+        $currencies = array(
+            [
+                "alpha3" => "eur",
+                "numeric" => "978",
+                "multiplicator" => 100
+            ]
+        );
 
-		);
-
-		// Uncomment the below to run the seeder
-		// DB::table('currencies')->insert($currencies);
-	}
-
+        // Uncomment the below to run the seeder
+        DB::table('currencies')->insert($currencies);
+    }
 }
